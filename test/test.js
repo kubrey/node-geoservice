98bd0f62df63b21c46e95bb08b6086d934640595
kubrey <kubrey@gmail.com>
2016-02-18 16:31:57 +0200

    Тесты+

---
+++ test/test.js
@@ -0,0 +1,95 @@
+"use strict";
+
+var assert = require("chai").assert;
+var geo = require('../app');
+
+describe('Geolocator common function', function () {
+    before(function () {
+        console.log('starting...');
+        geo.setOptions({});
+    });
+    it("getting ip data as object(valid ip)", function () {
+        geo.lookup('8.8.8.8', function (err, result) {
+            //console.log(err,result);
+            assert.isObject(result, "Return object");
+            //assert.isNull(err,true,"No error");
+        });
+    });
+    it("getting no error on valid ip", function () {
+        geo.lookup('8.8.8.8', function (err, result) {
+            //console.log(err,result);
+            assert.isNull(err, "No error");
+        });
+    });
+    it("getting error because of invalid ip", function () {
+        geo.lookup('8.8.8.888', function (err, result) {
+            console.log(err);
+            assert.isNotNull(err, "Got error");
+        });
+    });
+    it("getting error because no methods are allowed", function () {
+        geo.setOptions({
+            services: {
+                'ip-api': false,
+                'maxmind-dat': false,
+                'maxmind-mmdb': false,
+                'ipinfo': false,
+                'freegeoip': false,
+                'geobytes': false,
+                'ipgeobase': false
+            }
+        });
+        geo.lookup('8.8.8.8', function (err, result) {
+            console.log(err);
+            assert.isNotNull(err, "Got error");
+        });
+    });
+
+    it("Setting invalid service name for valid ip - service should be ignored", function () {
+        geo.setOptions({
+            services: {
+                'wrong': false
+            }
+        });
+        geo.lookup('8.8.8.8', function (err, result) {
+            //console.log(err,result);
+            assert.isNull(err, "No errors");
+        });
+    });
+    it("Setting invalid field name for valid ip - field should be ignored", function () {
+        geo.setOptions({
+            fields: {
+                wrong: true
+            }
+        });
+        geo.lookup('8.8.8.8', function (err, result) {
+            assert.isNull(err, "No errors");
+        });
+    });
+    it("Setting invalid common option for valid ip - option should be ignored", function () {
+        geo.setOptions({
+            common: {
+                wrong: 23
+            }
+        });
+        geo.lookup('8.8.8.8', function (err, result) {
+            assert.isNull(err, "No errors");
+        });
+    });
+
+    it("Double check level is too high - should return error", function () {
+        geo.setOptions({
+            fields: {
+                city: true
+            },
+            common: {
+                checkField: 'city',
+                checkLevel: 8
+            }
+        });
+        geo.lookup('8.8.8.8', function (err, result) {
+            console.log(err,result);
+            assert.isNotNull(err, "No data");
+        });
+    });
+});
