13cff746c5aef52dec9370d209e1f737fc4b49f9
kubrey <kubrey@gmail.com>
2016-04-11 12:51:35 +0300

    value of service activity is not strictly boolean + fix for data of maxmind-mmdb

--- examples/service.js
+++ examples/service.js
@@ -5,8 +5,11 @@ var ip = process.argv[2] || '8.8.8.8';
 var options = {
     services: {
         'maxmind-dat': false,
-        'maxmind-mmdb': false,
+        'maxmind-mmdb': 1,
         'ipgeobase': false,
+        'geobytes': false,
+        'freegeoip': false,
+        'ip-api':false,
         'ipinfo':false
     }
 };
